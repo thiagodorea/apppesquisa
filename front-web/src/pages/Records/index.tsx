9e4712c6bca42674548cb738444b042b83f43089
Thiago Dorea <thiago.dorea@hotmail.com>
2020-09-20 19:38:31 -0300

    Criando pagina de lstagem dos registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom'
+import './style.css';
+import { RecordsResponse } from './types'
+import { formatDate } from './helpers'
+import  Pagination  from './Pagination'
 
-const Records = () => (
-    <h1>records</h1>
-);
+const BASE_URL = 'http://10.146.23.24:8080'
+const Records = () => {
+
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+    const [activePage, setActivePage] = useState(0);
+
+    useEffect( ()=>{
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+        .then(resp => setRecordsResponse(resp.data))
+    }, [activePage]);
+
+    const handlePageChange = (index: number) => {
+        setActivePage(index);
+    }
+
+    return (
+        <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="/charts">
+                    <button className="action-filters">VER GRAFICO</button>
+                </Link>
+            </div>
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDATE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {recordsResponse?.content.map(record =>(
+                    <tr key={record.id}>
+                        <td>{formatDate(record.moment)}</td>
+                        <td>{record.name}</td>
+                        <td>{record.age}</td>
+                        <td className="text-secondary">{record.gamePlatform}</td>
+                        <td>{record.genreName}</td>
+                        <td className="text-primary">{record.gameTitle}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <Pagination 
+                activePage={activePage}
+                goToPage={handlePageChange}
+                totalPages={recordsResponse?.totalPages}
+            />
+        </div>
+    );
+}
 
 export default Records;
