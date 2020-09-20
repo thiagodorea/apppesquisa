e955ef1728200996707d228eb042e70a4e9fb0c9
Thiago Dorea <thiago.dorea@hotmail.com>
2020-09-20 14:07:29 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dspesquisa/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		// H2
+
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
